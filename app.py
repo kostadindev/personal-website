57934fb7757d64867a98171746a38f814caedf24
KostadinDev <48786151+KostadinDev@users.noreply.github.com>
2020-07-07 01:42:37 -1000

    using new template for website

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, redirect
-import csv
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def homepage():
     return render_template('index.html')
 
 
@@ -14,35 +13,5 @@ def html_page(page_name='index.html'):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email},{subject},{message}')
-
-
-def write_to_csv(data):
-    with open('database.csv', mode='a', newline='') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email, subject, message])
-
-
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form(page_name='index.html'):
-    if request.method == 'POST':
-        try:
-            data = request.form.to_dict()
-            write_to_csv(data)
-            return redirect('thank-you.html')
-        except:
-            return 'did not save to database'
-    else:
-        return 'error line 44 app.py'
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
